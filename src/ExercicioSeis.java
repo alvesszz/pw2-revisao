5b1f76132377822356f68db1d57dfcb7c69f7d27
Allan-Fabia <Allan-Fabia@test.com>
2022-03-28 10:46:47 -0300

    Desenvolvimento do exercicio Seis

---
+++ src/ExercicioSeis.java
@@ -0,0 +1,25 @@
+
+public class ExercicioSeis {
+	
+	public static void main(String[] args) {
+		
+		double AbaixodoPeso = 2;
+		double Acimadopeso = 58;
+		double Pesonormal = 6;
+		
+		if (AbaixodoPeso < 4)
+		{ 
+			System.out.println("Abaixo do peso");
+		}
+		if (Acimadopeso > 10)
+		{
+			System.out.println("Acima do peso");
+		}
+		if (Pesonormal > 4 && Pesonormal < 10)
+		{
+			System.out.println("Peso normal");
+		}
+			
+	}
+
+}
--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -1,8 +1,8 @@
 
 public class ExercicioUm {
 	public static void main(String[] args) {
-		String nome = "Jotave ", sobrenome = "Ramos ";
-		int idd = 16;
+		String nome = "Allan e Fabia", sobrenome = " ";
+		int idd = 16; 
 		String resultado = nome + sobrenome + "tem " + idd + " anos";
 		System.out.println("" + resultado);
 	}
