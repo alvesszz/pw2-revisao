6b5baae151d809b57b509c5bf38a3e528e63c054
Allan-Fabia <Allan-Fabia@test.com>
2022-03-28 11:01:28 -0300

    Desenvolvimento do Exercicio Oito

---
+++ src/ExercicioOito.java
@@ -0,0 +1,15 @@
+
+public class ExercicioOito {
+
+	public static void main(String[] args) {
+		
+		char diaSemana = 25;
+		
+		if (diaSemana >= 1 && diaSemana <= 5)
+			System.out.println("Você pode ir ao banco");
+		else 
+			System.out.println("Você não pode ir ao banco");
+		
+	}
+		
+}
