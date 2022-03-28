9595061cc01758ebbb836abef1a6c6cb5142e96a
Allan-Fabia <Allan-Fabia@test.com>
2022-03-28 10:54:00 -0300

    Desenvolvimento do exercicio Sete

---
+++ src/ExercicioSete.java
@@ -0,0 +1,19 @@
+
+public class ExercicioSete {
+
+	public static void main(String[] args) {
+		int triangulo = 24;
+		if ((triangulo * 3) / 3 == 10)
+		{
+			System.out.println(" Equilatero");
+			
+		}
+		if ((triangulo * 2) / 2 == 10)
+		{
+			System.out.println(" Isosceles");
+		}
+		else 
+			System.out.println(" Escaleno");
+		
+	}
+}
